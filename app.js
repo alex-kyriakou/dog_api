e70ba40b96a15bb55c287e408331cae722ac763b
Alexandros <alexandr.kyriakou@gmail.com>
2022-03-21 22:22:59 +0200

    List Select

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const allBreeds = "https://dog.ceo/api/breeds/list/all";
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+
+  breedList(data.message);
+}
+
+start();
+
+function breedList(breeds) {
+  Object.keys(breeds).map(function (breed) {});
+}
