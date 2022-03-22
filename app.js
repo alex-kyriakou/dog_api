0735fce0174f576d904d990a805eacf4e7c22a71
Alexandros <alexandr.kyriakou@gmail.com>
2022-03-22 22:33:59 +0200

    Next Slide and currentposition

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+let timer;
+let deleteFirstPhotoDeleay;
+
 // Fetching data from API
 async function start() {
-  const response = await fetch("https://dog.ceo/api/breeds/list/all");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await response.json();
 
-  createBreedList(data.message);
+    createBreedList(data.message);
+  } catch (e) {
+    console.log("There was a problem fetching the breed list");
+  }
 }
 
 start();
@@ -24,8 +31,51 @@ function createBreedList(breedList) {
 
 // Looad by Breed
 async function loadByBreed(breed) {
-  const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
-  const data = await response.json();
+  let URL = `https://dog.ceo/api/breed/${breed}/images`;
 
-  console.log(data);
+  if (breed != "Choose a Dog breed") {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    createSlideshow(data.message);
+  }
+}
+
+function createSlideshow(images) {
+  let currentPosition = 0;
+  clearInterval(timer);
+  clearTimeout(deleteFirstPhotoDeleay);
+
+  if (images.length > 1) {
+    document.getElementById("slideshow").innerHTML = `
+  <div  class="slide" style=" background-image: url('${images[0]}')"></div>
+  <div  class="slide" style=" background-image: url('${images[1]}')"></div>
+  `;
+
+    currentPosition += 2;
+    if (images.length == 2) currentPosition = 0;
+    timer = setInterval(nextSlide, 3000);
+  } else {
+    document.getElementById("slideshow").innerHTML = `
+  <div  class="slide" style=" background-image: url('${images[0]}')"></div>
+  <div  class="slide"></div>
+  `;
+  }
+
+  function nextSlide() {
+    document
+      .getElementById("slideshow")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<div  class="slide" style=" background-image: url('${images[currentPosition]}')"></div>`
+      );
+    deleteFirstPhotoDeleay = setTimeout(function () {
+      document.querySelector(".slide").remove();
+    }, 1000);
+    if (currentPosition + 1 >= images.length) {
+      currentPosition = 0;
+    } else {
+      currentPosition++;
+    }
+  }
 }
