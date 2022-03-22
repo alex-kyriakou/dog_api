9f318b7b3819747391fbc1add7e09945b175b013
Alexandros <alexandr.kyriakou@gmail.com>
2022-03-22 20:48:30 +0200

    JS select breed list

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+// Fetching data from API
 async function start() {
   const response = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await response.json();
 
-  breedList(data.message);
+  createBreedList(data.message);
 }
 
 start();
 
-function breedList(breeds) {
-  Object.keys(breeds).map(function (breed) {});
+// Create the Select list with all breeds through the data of API
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+    <option>Choose a Dog breed</option>
+    ${Object.keys(breedList)
+      .map(function (breed) {
+        return `<option>${breed}</option>`;
+      })
+      .join("")}
+  </select>
+  `;
+}
+
+// Looad by Breed
+async function loadByBreed(breed) {
+  const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+  const data = await response.json();
+
+  console.log(data);
 }
